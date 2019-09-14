4e85e6c89fb7931b51562b00f881ef62e6e6f973
Mal <malrsteffes@gmail.com>
2019-09-14 11:40:15 -0400

    initialized firebase and began creating some global variables

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    // My web app's Firebase configuration
+    var firebaseConfig = {
+    apiKey: "AIzaSyBN3qRSwmTv8ZOXc_IWDG90i5Nhl-BFzW8",
+    authDomain: "rpsmultiplayer-5a8a8.firebaseapp.com",
+    databaseURL: "https://rpsmultiplayer-5a8a8.firebaseio.com",
+    projectId: "rpsmultiplayer-5a8a8",
+    storageBucket: "",
+    messagingSenderId: "984519419568",
+    appId: "1:984519419568:web:921db4a7d1109a75be3d75"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+})
