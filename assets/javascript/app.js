eb011a4877a1478f75ed83aed36b90bc1bd1056b
Mal <malrsteffes@gmail.com>
2019-09-14 12:15:53 -0400

    successfully storing player 1 user info in firebase. now working on getting the program to cap out at 2 and begin the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,4 +12,44 @@ $(document).ready(function () {
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
+
+    // Create a variable to refer to the Firebase Database
+    var database = firebase.database();
+
+    // Create global variables. First we need a way to check if we have enough players.
+    var playerCount = 0;
+    var checkP1 = false;
+    var checkP2 = false;
+
+    var p1Wins;
+    var p1Losses;
+    var p1Name;
+    var p1Choice;
+
+    var p2Wins;
+    var p2Losses;
+    var p2Name;
+    var p2Choice;
+
+
+    // Submits and stores our player's info in the database
+    $('#playerSend').on('click', function(){
+        // Prevent the page from refreshing
+        event.preventDefault();
+        if (playerCount === 0){
+            playerCount++;
+            p1Name = $("#playerName").val().trim();
+            var newPlayer = database.ref('player').child(playerCount);
+
+            newPlayer.set({
+                name: p1Name,
+                wins: 0,
+                losses: 0,
+              });
+        }
+
+        // Hide the input section
+		$('#playerInput').hide();
+
+	})
 })
